7264cc0d658cda1d2b861e80549aac02f8ec35c8
Nick Davies <nbdavies@gmail.com>
2016-02-15 15:20:59 -0600

    Update user.rb

--- pokepedia/app/models/user.rb
+++ pokepedia/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :articles
-  has_many :versions, foreign_key: :editor_id
+  has_many :versions, foreign_key: :author_id
   has_many :edited_articles, through: :versions, source: :articles
   has_secure_password
 
