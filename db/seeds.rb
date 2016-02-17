57391626431b583a04bc1551a14ff4280812757a
Alex Mitzman & Nick Davies <pair+amitzman.nbdavies@devbootcamp.com>
2016-02-17 13:58:50 -0600

    Add some GUY

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ end
 Version.create!(article_id: article1.id, author: User.all.sample, content: Faker::Hipster.paragraphs(5).join("\n"), published: true, image_url: "https://imgflip.com/s/meme/Creepy-Condescending-Wonka.jpg", image_caption: "This is Sparta"  )
 
 pokemons = ["pikachu","charmander","squirtle","geodude","jynx","eevee","abra","ponyta","poliwag", "alakazam", "nidoran", "charizard", "zapdos", 'magmar', "vulpix", "weedle", "jigglypuff", "machop", "machamp", "haunter", "ditto", "bulbasaur", "dragonite", "pidgey", "nick", "kirby", "ice climbers", "team rocket", "ash", "muk"]
-pokemon_image = ["https://images-na.ssl-images-amazon.com/images/G/01/videogames/detail-page/pokemon.blk-wte.oshawatt.lg.jpg", "http://blogs-images.forbes.com/davidewalt/files/2012/10/pikachu-pokemon.png", "http://img12.deviantart.net/50fb/i/2010/163/5/5/pokemon_traces_007_squirtle_by_riseagainstlife.jpg"]
+pokemon_image = ["https://images-na.ssl-images-amazon.com/images/G/01/videogames/detail-page/pokemon.blk-wte.oshawatt.lg.jpg", "http://blogs-images.forbes.com/davidewalt/files/2012/10/pikachu-pokemon.png", "http://img12.deviantart.net/50fb/i/2010/163/5/5/pokemon_traces_007_squirtle_by_riseagainstlife.jpg", "http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2013/9/30/0/ITK_guy-fieri-diners-drive-ins-and-dives_s3x4.jpg", "http://vignette2.wikia.nocookie.net/universe-of-smash-bros-lawl/images/a/a3/Guy_Fieri.png/revision/latest?cb=20160128070849"]
 pokemons.each do |pokemon|
   article = Article.create!(title: pokemon, author: User.all.sample)
     2.times do
