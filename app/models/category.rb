dacf2c7768b1c62aa8852223c76527bc94b5038b
Nick Davies <nbdavies@gmail.com>
2016-02-16 20:41:52 -0600

    Add category page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,14 @@ class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :versions, through: :categorizations
   validates :name, presence: true, uniqueness: true
+
+  def articles
+  	versions = self.versions
+  	versions = versions.to_a.select! do |version|
+  		version == version.article.published_version
+  	end
+  	versions.map! do |version|
+  		version.article
+  	end
+  end
 end
