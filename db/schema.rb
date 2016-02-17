aa9b91ade3c86a033761540db3b61c494a4d756d
Nick Davies <nbdavies@gmail.com>
2016-02-16 21:32:52 -0600

    Add Flag button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215205413) do
+ActiveRecord::Schema.define(version: 20160217030331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160215205413) do
     t.integer  "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "flagged"
   end
 
   create_table "categories", force: :cascade do |t|
